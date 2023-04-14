f67f4c5deff36c9391d5b8866505a65c9b8721b4
Arjun J. Ram <arjunjram@gmail.com>
2023-04-13 18:56:38 -0700

    Small changes

--- C++/examples/lcqp-ipopt/main.cpp
+++ C++/examples/lcqp-ipopt/main.cpp
@@ -29,23 +29,24 @@ int main() {
 
     IPOPT<double, NX, NG, NH> ipopt(lcqp_1, mu);
     
-    Eigen::Matrix<double, NX, 1> x0 = (Eigen::Matrix<double, NX, 1>::Ones()*3.).eval();
-    Eigen::Matrix<double, NG, 1> sigma = -mu*lcqp_1.inequalityConstraintVector(x0).value().cwiseInverse();
+    Eigen::Matrix<double, NX, 1> x_soln = (Eigen::Matrix<double, NX, 1>::Ones()*3.).eval();
+    Eigen::Matrix<double, NG, 1> sigma = -mu*lcqp_1.inequalityConstraintVector(x_soln).value().cwiseInverse();
     Eigen::Matrix<double, NH, 1> lambda = Eigen::Matrix<double, NH, 1>::Ones();
 
     for (size_t num = 0; num < NUM_ITER; num++) {
-        const Eigen::Matrix<double, NX+NG+NH, 1> x_opt = ipopt.solve(x0, sigma, lambda);
+        const Eigen::Matrix<double, NX+NG+NH, 1> x_opt = ipopt.solve(x_soln, sigma, lambda);
 
-        x0.block<NX, 1>(0, 0) = x_opt.block<NX, 1>(0, 0).eval();
+        x_soln.block<NX, 1>(0, 0) = x_opt.block<NX, 1>(0, 0).eval();
         sigma.block<NG, 1>(0, 0) = x_opt.block<NG, 1>(NX, 0).eval();
+        lambda.block<NH, 1>(0, 0) = x_opt.block<NH, 1>(NX+NG, 0).eval();
 
         mu *= 0.8;
 
         ipopt.updateMu(mu);
     }
 
-    std::cout << "Objective function: " << lcqp_1.objectiveFunction(x0) << std::endl;
-    std::cout << "Optimized solution:\n" << x0 << std::endl;
+    std::cout << "Objective function: " << lcqp_1.objectiveFunction(x_soln) << std::endl;
+    std::cout << "Optimized solution:\n" << x_soln << std::endl;
     std::cout << "Sigma:\n" << sigma << std::endl;
     std::cout << "Lambda:\n" << lambda << std::endl;
 
